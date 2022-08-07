51d759189fe5825b1d819fe8319fca874cb4ef0b
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-07 23:47:48 +0530

    Add LoginFormController

---
+++ CoueseWork-Project/src/LoginFormController.java
@@ -0,0 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public Button btnLogIn;
+
+    public void btnLogInOnAction(ActionEvent actionEvent) {
+    }
+}
