e65c11b60592fe1224ea07b30eefda5b64d3d3df
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-09 20:03:55 +0530

    Set login form content

--- CoueseWork-Project/src/controller/LoginFormController.java
+++ CoueseWork-Project/src/controller/LoginFormController.java
@@ -1,13 +1,21 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public TextField txtUserName;
     public Button btnLogIn;
 
-    public void btnLogInOnAction(ActionEvent actionEvent) {
+    public void btnLogInOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage=new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        stage.show();
     }
 }
