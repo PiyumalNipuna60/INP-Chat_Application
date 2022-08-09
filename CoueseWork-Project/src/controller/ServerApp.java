21fe3856faa66a886f84e4e0d2e86ffa0d134946
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-09 23:00:28 +0530

    Set ServerApp Class

---
+++ CoueseWork-Project/src/controller/ServerApp.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ServerApp {
+    public Button btnSent;
+    public TextArea txtAreaMsg;
+    public TextField txtMsg;
+
+    public void btnSentOnAction(ActionEvent actionEvent) {
+    }
+}
