24c4c6d26958d2dc1fb0317afa654fdfbf6bf562
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-09 23:22:03 +0530

    Set ClientApp_01 Class

---
+++ CoueseWork-Project/src/controller/ClientApp01.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ClientApp01 {
+    public Button btnSent;
+    public TextField txtMsg;
+    public TextArea txtAreaMsg;
+
+    public void btnSentOnAction(ActionEvent actionEvent) {
+    }
+}
