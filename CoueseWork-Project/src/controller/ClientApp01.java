0fd08dfcce12e6a8a71bfef13d654b93056868e5
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-10 22:05:03 +0530

    Client-01 : Set Initialize rint

--- CoueseWork-Project/src/controller/ClientApp01.java
+++ CoueseWork-Project/src/controller/ClientApp01.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -14,13 +16,24 @@ public class ClientApp01 {
     public TextArea txtAreaMsg;
     Socket socket;
     final int PORT=9000;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
 
+    String massage="", reply="";
     public void initialize(){
         new Thread(()->{
             try {
                 socket=new Socket("localhost",PORT);
+                txtAreaMsg.appendText("Accept Client..!");
+                txtAreaMsg.appendText("\n.............................................\n");
 
+                dataOutputStream=new DataOutputStream(socket.getOutputStream());
+                dataInputStream=new DataInputStream(socket.getInputStream());
 
+                while ("Exit".equals(txtMsg.getText())){
+                 massage=dataInputStream.readUTF();
+                 txtAreaMsg.appendText("\nServer : " + massage);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
