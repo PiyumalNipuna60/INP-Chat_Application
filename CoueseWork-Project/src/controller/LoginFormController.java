3f69f8b4a0605c6222b040706925a3d4ebc6fbc9
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-10 14:08:09 +0530

    set loginfrom btnLogInOnAction content

--- CoueseWork-Project/src/controller/LoginFormController.java
+++ CoueseWork-Project/src/controller/LoginFormController.java
@@ -14,8 +14,24 @@ public class LoginFormController {
     public Button btnLogIn;
 
     public void btnLogInOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage=new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ServerApp.fxml"))));
         stage.show();
+
+        if ("kasun".equals(txtUserName.getText())) {
+            Stage stage2 = new Stage();
+            stage2.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientApp_01.fxml"))));
+            stage2.show();
+
+        }else if("nipuna".equals(txtUserName.getText())){
+            Stage stage2 = new Stage();
+            stage2.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientApp_02.fxml"))));
+            stage2.show();
+
+        }else if("sadun".equals(txtUserName.getText())){
+            Stage stage2 = new Stage();
+            stage2.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientApp_03.fxml"))));
+            stage2.show();
+        }
     }
 }
